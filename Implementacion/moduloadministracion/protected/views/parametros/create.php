ee21c48fca606bea241a87c21b3e2fb5220d0a2e
esteban1306 <esteban1306@gmail.com>
2014-11-10 15:48:31 -0500

    Detalles con nombres

--- Implementacion/moduloadministracion/protected/views/parametros/create.php
+++ Implementacion/moduloadministracion/protected/views/parametros/create.php
@@ -13,6 +13,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Create Parametros</h1>
+<h1>Crear Parametro</h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
--- Implementacion/moduloadministracion/protected/views/parametros/view.php
+++ Implementacion/moduloadministracion/protected/views/parametros/view.php
@@ -16,7 +16,7 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Parametros #<?php echo $model->id; ?></h1>
+<h1> Parametro #<?php echo $model->Nombre; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
