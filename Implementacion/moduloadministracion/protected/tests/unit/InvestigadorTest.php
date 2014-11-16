72fb045d3a9f0c00351d3a58fd25a94cecd07af2
Andres Fernando López Avila <andresflopez@icloud.com>
2014-11-15 19:53:27 -0500

    cambio de la interfaz

--- Implementacion/moduloadministracion/protected/tests/unit/InvestigadorTest.php
+++ Implementacion/moduloadministracion/protected/tests/unit/InvestigadorTest.php
@@ -1,8 +1,8 @@
 <?php
 class InvestigadorTest extends PHPUnit_Framework_TestCase
 {
-	 public function testCreate()
-	 {
+	//test para el investigador 
+	 public function testCreateInvestigador(){
 	 	$investigador= new Investigador;
 	 	$investigador->Nombres="Pedro";
 	 	$investigador->Apellidos="Perez";
@@ -14,19 +14,74 @@ class InvestigadorTest extends PHPUnit_Framework_TestCase
 	 	$this->assertTrue($investigador->save());
 	 }
 
-	 public function testBuscar()
-	 {
+	 public function testBuscarInvestigador(){
 	 	$investigador = new Investigador;
 	 	$aux = $investigador->findByPk(1094941728);
 	 	$this->assertNotNull($aux);
 	 }
 
-	 public function testEliminar(){
+	 public function testEliminarInvestigador(){
 	 	$investigador = new Investigador;
 	 	$aux = $investigador->findByPk(1094941728);
 	 	$aux->delete();
 	 	$aux = $investigador->findByPk(1094941728);
 	 	$this->assertNull($aux);
+	 }
+
+	 //test para las organizaciones
+	 public function testCrearOrganizacion(){
+	 	$organizacion= new organizaciones;
+	 	$organizacion->Nit=1010;
+	 	$organizacion->Email="example@hotmail.com";
+	 	$organizacion->Direccion="Mercedez";
+	 	$organizacion->Telefono="7460000";
+	 	$organizacion->Nombre="ComeCon";
+	 	$organizacion->motivoCambioOrganizacion="Por chimba";
+	 	$organizacion->Ciudad="Armenia";
+	 	$organizacion->validar="yes";
+	 	$this->assertTrue($organizacion->save());
+	 }
+
+	 public function testBuscarOrganizacion(){
+	 	$organizacion=new Organizaciones;
+	 	$aux=$organizacion->findByPk(1010);
+	 	$this->assertNotNull($aux);
+	 }
+
+	 public function testEliminarOrganizacion(){
+	 	$organizacion=new Organizaciones;
+	 	$aux=$organizacion->findByPk(1010);
+	 	$aux->delete();
+	 	$aux=$organizacion->findByPk(1010);
+	 	$this->assertNull($aux);
+	 }
+
+	 //test para los parametros
+	 public function testCrearParametro(){
+	 	$parametro=new Parametros;
+	 	$parametro->id=10;
+	 	$parametro->Nombre="prueba";
+	 	$parametro->Valor="value";
+	 	$this->assertTrue($parametro->save());
+	 }
+
+	 public function testBuscarParametro(){
+	 	$parametro=new Parametros;
+	 	$aux=$parametro->findByPk(10);
+	 	$this->assertNotNull($aux);
+	 }
+
+	 public function testEliminarParametro(){
+	 	$parametro=new Parametros;
+	 	$aux=$parametro->findByPk(10);
+	 	$aux->delete();
+	 	$aux=$parametro->findByPk(10);
+	 	$this->assertNull($aux);
+	 }
+
+	 //test para las plantillas
+	 public function testPlantillas(){
+	 	$plantilla
 	 }
 }
 ?>
